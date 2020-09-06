d9ac055ee8b331fa45dd7a80fcd1e01415ad854d
Erick Kasih <erick.kasih@gmail.com>
2020-09-06 07:35:30 +0700

    Initial Commit

---
+++ Mareco/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mareco
+//
+//  Created by JAN FREDRICK on 06/09/20.
+//  Copyright © 2020 JFSK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
